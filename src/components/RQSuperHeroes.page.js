6d435463e58c11077e33f844bfe37afc49bc9fa3
Monika-Sivakumar-3 <monika.sivakumar@thoughtworks.com>
2023-09-18 23:03:23 +0530

    Fetch data on a button click

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,9 +2,16 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery("super-heroes", () => {
-    return axios.get("http://localhost:4000/superheroes");
-  });
+  const { isLoading, data, isError, error, refetch } = useQuery(
+    "super-heroes",
+    () => {
+      return axios.get("http://localhost:4000/superheroes");
+    },
+    {
+      //this will disable the default fetch call
+      enabled: false,
+    }
+  );
 
   if (isError) return <h2>{error.message}</h2>;
   if (isLoading) return <h1>Loading...</h1>;
@@ -14,6 +21,8 @@ export const RQSuperHeroesPage = () => {
       {data?.data.map((hero) => (
         <div>{hero.name}</div>
       ))}
+      {/* We can enable the call by executing the refetch function which we get fron useQuery function */}
+      <button onClick={refetch}>Fetch SuperHeroess</button>
     </>
   );
 };
