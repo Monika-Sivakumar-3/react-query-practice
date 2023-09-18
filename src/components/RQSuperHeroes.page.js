e1ec3094bb74afedc9610f0bdeeea18d6e1898c7
Monika-Sivakumar-3 <monika.sivakumar@thoughtworks.com>
2023-09-18 22:53:17 +0530

    Polling

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,9 +2,22 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery("super-heroes", () => {
-    return axios.get("http://localhost:4000/superheroes");
-  });
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    () => {
+      return axios.get("http://localhost:4000/superheroes");
+    },
+    {
+      //Say you have a server where data gets updated frequently we can using polling to refetch the data at a regular interval
+      //Eg: Stock price
+      //We can do it using polling(refetchInterval) by default it is false
+      //API call will have frequently at the given interval
+      //When the window loose focus the call will not happen
+      refetchInterval: 2000,
+      //To fetch data in BG even when the screen is not focused
+      refetchIntervalInBackground: true,
+    }
+  );
 
   if (isError) return <h2>{error.message}</h2>;
   if (isLoading) return <h1>Loading...</h1>;
