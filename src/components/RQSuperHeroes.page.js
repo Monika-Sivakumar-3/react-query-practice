2c2510ab74b6d417151d0b8b53611c481b5b0a04
Monika-Sivakumar-3 <monika.sivakumar@thoughtworks.com>
2023-09-18 22:31:49 +0530

    Remove comments

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,25 +2,9 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery(
-    "super-heroes",
-    () => {
-      return axios.get("http://localhost:4000/superheroes");
-    },
-    {
-      //Usually when you go to a tab it fetches the data
-      //If we set the stale time it won't and it's because by default refetchOnMount: true
-      //If we set it to --false-- it won't fetch the data except for the first time
-      //If we set it to --always-- it will fetch the data always even if we have stale time
-      refetchOnMount: true,
-      //Change something in db.json and come back to the page and check
-      //In traditional method we can't see the updated data but in RQ we can see the updated data even if we have stale time
-      //This is because refetchOnWindowFocus: 'always'
-      //If we set it to false the updated data won't be visible until we refresh the screen
-      //If we set it to true the updated data will be visible but not during the staleTime duration
-      refetchOnWindowFocus: "always",
-    }
-  );
+  const { isLoading, data, isError, error } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
 
   if (isError) return <h2>{error.message}</h2>;
   if (isLoading) return <h1>Loading...</h1>;
