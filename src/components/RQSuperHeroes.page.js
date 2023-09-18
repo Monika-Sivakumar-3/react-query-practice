bc28d471c8d7d630d77bf5d1647064667da3d2f5
Monika-Sivakumar-3 <monika.sivakumar@thoughtworks.com>
2023-09-18 08:28:48 +0530

    Add. useQuery hook to fetch data from API

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,22 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
 export const RQSuperHeroesPage = () => {
-  return <div>RQSuperHeroes Page</div>;
+  //useQuery hook from react-query is used to fetch data from server
+  //it takes two argument one is the unique key(see the imp later) and the other one is a call back function which returns promise
+  //Remember: In the previous example(super heroes file) we used two states loading and data to determine the data feched result and to display it
+
+  //We can also get more infos like isLoading from useQuery
+  const { isLoading, data } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
+  if (isLoading) return <h1>Loading...</h1>;
+  return (
+    <>
+      <h2>RQSuperHeroes Page</h2>
+      {data?.data.map((hero) => (
+        <div>{hero.name}</div>
+      ))}
+    </>
+  );
 };
