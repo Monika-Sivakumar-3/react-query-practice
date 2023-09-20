94e00466ddfae068dbd835fc2016d8150da0e8dc
Monika-Sivakumar-3 <monika.sivakumar@thoughtworks.com>
2023-09-20 22:19:22 +0530

    Initial query data

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,14 +1,25 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
-export const useSuperHeroData = (superHeroId, attributes) => {
+export const useSuperHeroData = (heroId) => {
+  const queryClient = useQueryClient();
   return useQuery(
-    ["super-heroes", superHeroId],
-    () => {
-      return axios.get(`http://localhost:4000/superheroes/${superHeroId}`);
+    ["super-hero", heroId],
+    ({ queryKey }) => {
+      const heroId = queryKey[1];
+      return axios.get(`http://localhost:4000/superheroes/${heroId}`);
     },
     {
-      ...attributes,
+      initialData: () => {
+        const hero = queryClient
+          .getQueryData("super-heroes")
+          ?.data?.find((hero) => hero.id === parseInt(heroId));
+        if (hero) {
+          return { data: hero };
+        } else {
+          return undefined;
+        }
+      },
     }
   );
 };
