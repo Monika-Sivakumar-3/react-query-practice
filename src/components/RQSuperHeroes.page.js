8e287f52bbd165bce996f02d7b8901a01f889937
Monika-Sivakumar-3 <monika.sivakumar@thoughtworks.com>
2023-09-21 08:21:41 +0530

    Optimistic updates

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,28 +1,57 @@
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  useAddSuperHeroData,
+  useSuperHeroesData,
+} from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
-  //This is good for our app but it is not so effecient for larger Apps
-  //What is more components need this data
-  //We will have to rewrite this code in all those components
-  //To avoid this we are going to create a custom hook
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
 
   const { isLoading, data, isError, error, refetch } = useSuperHeroesData({
     enabled: false,
     onSuccess: (data) => console.log("Success", data),
   });
 
+  const { mutate: addHero } = useAddSuperHeroData({
+    onError: (error) => {
+      console.error("Error adding hero:", error);
+    },
+  });
+
+  const handleAddHeroClick = () => {
+    console.log("Add Hero button clicked");
+    addHero({ name, alterEgo });
+  };
+
   if (isError) return <h2>{error.message}</h2>;
   if (isLoading) return <h1>Loading...</h1>;
   return (
     <>
-      <h2>RQSuperHeroes Page</h2>
-      {data?.data.map((hero) => (
-        <>
-          <a href={`/rq-super-heroes/${hero.id}`}>{hero.name}</a>
-          <br />
-        </>
-      ))}
-      <button onClick={refetch}>Fetch</button>
+      <h2>React Query Super Heroes Page</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
+
+      {data?.data.map((hero) => {
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+        );
+      })}
+      <button onClick={refetch}>Fetch heroes</button>
     </>
   );
 };
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 export const useSuperHeroesData = (attributes) => {
   return useQuery(
@@ -11,4 +11,41 @@ export const useSuperHeroesData = (attributes) => {
       ...attributes,
     }
   );
+};
+
+const addSuperHero = (hero) => {
+  return axios.post("http://localhost:4000/superheroes", hero);
+};
+
+export const useAddSuperHeroData = (attributes) => {
+  const queryClient = useQueryClient();
+
+  return useMutation(addSuperHero, {
+    onSuccess: (data) => {
+      // console.log("Successsss", data);
+      // queryClient.invalidateQueries("super-heroes");
+    },
+    //getting the data from the API and setting it to the cache so that we get the data spontaneously
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const previousHeroData = queryClient.getQueryData("super-heroes");
+      queryClient.setQueryData("super-heroes", (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
+        };
+      });
+      return { previousHeroData };
+    },
+    onError: (_err, _newTodo, context) => {
+      queryClient.setQueryData("super-heroes", context.previousHeroData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries("super-heroes");
+    },
+    ...attributes,
+  });
 };
